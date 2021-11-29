87f12bbc540f7c0c621545836d7584c34a2980bf
Simone Contorno <83269499+simone-contorno@users.noreply.github.com>
2021-11-29 10:13:52 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-unzip sources.zip -d $1
 mkdir executables
-gcc $1/sources/process_1/command_console.c -o executables/command_console &
-gcc $1/sources/process_2/inspection_console.c -o executables/inspection_console &
-gcc $1/sources/process_3/motor_x.c -o executables/motor_x &
-gcc $1/sources/process_4/motor_z.c -o executables/motor_z &
-gcc $1/sources/process_5/watchdog.c -o executables/watchdog &
+gcc src/process1/command_console.c -o executables/command_console &
+gcc src/process2/inspection_console.c -o executables/inspection_console &
+gcc src/process3/motor_x.c -o executables/motor_x &
+gcc src/process4/motor_z.c -o executables/motor_z &
+gcc src/process5/watchdog.c -o executables/watchdog &
